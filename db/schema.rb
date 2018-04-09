9b9f5fe647865261d73e8d54bb3f1ff809ca87cd
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-04-09 14:03:06 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409135440) do
+ActiveRecord::Schema.define(version: 20140212205318) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-# Could not dump table "rides" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "rides", force: :cascade do |t|
+    t.integer  "passenger_id"
+    t.integer  "taxi_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "taxis", force: :cascade do |t|
     t.datetime "created_at", null: false
